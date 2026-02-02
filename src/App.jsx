bbdc9be9bc40e6bcf16f75925fdaa586142bb472
Andrea Altomare <164928731+andrea-340@users.noreply.github.com>
2026-02-02 21:27:29 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,17 +6,10 @@ function App() {
   return (
     <Router>
       <Routes>
-        {/* Questa è la pagina che vedranno tutti i clienti */}
         <Route path="/" element={<ChatCliente />} />
-
-        {/* Questa è la tua pagina segreta (cambia il nome come vuoi) */}
-        <Route path="/tempio-segreto-creator-99" element={<DashboardAdmin />} />
-        
-        {/* Rotta di cortesia se sbagliano link */}
-        <Route path="*" element={<div style={{color: 'white', textAlign: 'center', marginTop: '50px'}}>L'universo non ha trovato questa pagina...</div>} />
+        <Route path="/admin-segreto" element={<DashboardAdmin />} />
       </Routes>
     </Router>
   );
 }
-
 export default App;
