5037f0111e247f2c5c3ba2930103a39b7f09f3f4
Andrea Altomare <164928731+andrea-340@users.noreply.github.com>
2026-02-02 21:13:05 +0100

    Implement routing with React Router in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,21 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import ChatCliente from './components/ChatCliente';
 import DashboardAdmin from './components/DashboardAdmin';
 
 function App() {
-  // Controlla se nell'URL c'è "?admin=true"
-  const isAdmin = new URLSearchParams(window.location.search).get('admin') === 'true';
-
   return (
-    <div className="App">
-      {isAdmin ? <DashboardAdmin /> : <ChatCliente />}
-    </div>
+    <Router>
+      <Routes>
+        {/* Questa è la pagina che vedranno tutti i clienti */}
+        <Route path="/" element={<ChatCliente />} />
+
+        {/* Questa è la tua pagina segreta (cambia il nome come vuoi) */}
+        <Route path="/tempio-segreto-creator-99" element={<DashboardAdmin />} />
+        
+        {/* Rotta di cortesia se sbagliano link */}
+        <Route path="*" element={<div style={{color: 'white', textAlign: 'center', marginTop: '50px'}}>L'universo non ha trovato questa pagina...</div>} />
+      </Routes>
+    </Router>
   );
 }
 
