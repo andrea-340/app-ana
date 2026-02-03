c495da1398ccd355a6c0665fa6e926cd6b2b7e74
Andrea Altomare <164928731+andrea-340@users.noreply.github.com>
2026-02-03 03:34:45 +0100

    Update ChatCliente.jsx

--- src/components/ChatCliente.jsx
+++ src/components/ChatCliente.jsx
@@ -10,7 +10,7 @@ export default function ChatCliente() {
 
   // RISORSE ESTERNE
   const bgImage = "https://images.unsplash.com/photo-1514897575457-c4db467cf78e?auto=format&fit=crop&q=80&w=2000"; // Sfondo magico scuro
- const fotoCartomante = "/assets/anastasia.jpg";
+ const fotoCartomante = "https://github.com/andrea-340/app-ana/blob/main/src/assets/anastasia.jpg";
   const tiktokUrl = "https://www.tiktok.com/@anastasia.lapiubella?_r=1&_t=ZN-93bLbLeYkBa";
   const tiktokLogo = "https://cdn-icons-png.flaticon.com/512/3046/3046121.png";
 
