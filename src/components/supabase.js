c8bb89f790e9a27bb6e0b5774ec0cf5dc9190e19
Andrea Altomare <164928731+andrea-340@users.noreply.github.com>
2026-03-03 14:08:57 +0100

    Update Supabase key in supabase.js

--- src/components/supabase.js
+++ src/components/supabase.js
@@ -2,6 +2,6 @@
 import { createClient } from "@supabase/supabase-js";
 
 const SUPABASE_URL = "https://kejmjzkegmdhjaduozgw.supabase.co";
-const SUPABASE_KEY = "sb_publishable_X0QukhHY4N_w1GTWY9r50g_Q1QTTnCN";
+const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imtlam1qemtlZ21kaGphZHVvemd3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI1MzAyMDQsImV4cCI6MjA4ODEwNjIwNH0.ek0aIQd5j7Wtn8Lvu1xBsb2TTLWXdCEt-Kjdkm8yS6Q";
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
