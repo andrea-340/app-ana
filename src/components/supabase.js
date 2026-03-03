eac6212f01686df403d03ba073032c8667538133
Andrea Altomare <164928731+andrea-340@users.noreply.github.com>
2026-03-03 14:41:14 +0100

    Delete src directory

--- src/components/supabase.js
+++
@@ -1,7 +0,0 @@
-// src/supabase.js
-import { createClient } from "@supabase/supabase-js";
-
-const SUPABASE_URL = "https://kejmjzkegmdhjaduozgw.supabase.co";
-const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imtlam1qemtlZ21kaGphZHVvemd3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI1MzAyMDQsImV4cCI6MjA4ODEwNjIwNH0.ek0aIQd5j7Wtn8Lvu1xBsb2TTLWXdCEt-Kjdkm8yS6Q";
-
-export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
