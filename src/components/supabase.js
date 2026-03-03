0122c833f778f96eb2d3eca5878a0d36fa3bd445
Andrea Altomare <164928731+andrea-340@users.noreply.github.com>
2026-03-03 14:42:58 +0100

    Add files via upload

---
+++ src/components/supabase.js
@@ -0,0 +1,8 @@
+import { createClient } from "@supabase/supabase-js";
+
+const SUPABASE_URL = "https://kejmjzkegmdhjaduozgw.supabase.co";
+// USA LA CHIAVE LUNGA eyJ...
+const SUPABASE_KEY =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imtlam1qemtlZ21kaGphZHVvemd3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI1MzAyMDQsImV4cCI6MjA4ODEwNjIwNH0.ek0aIQd5j7Wtn8Lvu1xBsb2TTLWXdCEt-Kjdkm8yS6Q";
+
+export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
